5ef221c77ec6f5b9a5d936408f7ca407cd39b3e2
gerasimenkoab <gerasimenko.alexander@gmail.com>
2023-03-27 18:09:16 +0200

    part 2

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,28 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
+def index_1():
+    user = {"username":"Vasili"}
+    elem_list = [
+        {
+            "number":1,
+            "person":{"username":"Vasili"},
+            "msg":"I am turtle!"
+        },
+        {
+            "number":2,
+            "person":{"username":"Petrovich"},
+            "msg":"Que pasa?"
+        }      
+    ]
+    html_code = render_template('index.html', title ="My", user = user, phraselist = elem_list)
+    return(html_code)
 
-def index():
-    return('Hello buddy!')
+@app.route('/index')
+def index_2():
+    return(render_template("index.html",title="The best", user ={"username":"Pepega"}))
+
+@app.route('/shmindex')
+def index_3():
+    return(render_template("base_template.html"))
