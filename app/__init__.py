8696a298e1b11ffd050a91c4b30221792d0009b5
gerasimenkoab <gerasimenko.alexander@gmail.com>
2023-03-30 18:25:16 +0200

    database

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app) # creating database instance for app
+from app import models
 migrate = Migrate(app, db) # creating db migration instance for app and db object
 
 from app import routes
