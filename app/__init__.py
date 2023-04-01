d9a0855e1c5b186ddb2715802691680ec6288440
gerasimenkoab <gerasimenko.alexander@gmail.com>
2023-04-01 02:27:35 +0200

    login implement

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask_login import LoginManager
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app) # creating database instance for app
 from app import models
 migrate = Migrate(app, db) # creating db migration instance for app and db object
+login = LoginManager(app)
 
 from app import routes
 
