483f750681e805b30967c5941da036830a7950f0
gerasimenkoab <gerasimenko.alexander@gmail.com>
2023-04-01 05:11:46 +0200

    part 5

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,18 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app
-from app.forms import LoginForm
+from app import app,db
+from app.forms import LoginForm,RegistrationForm
 from flask_login import current_user, login_user, logout_user
 from flask_login import login_required
 from app.models import User
 from werkzeug.urls import url_parse
 
 @app.route('/')
-def index():
-    return(render_template("index.html", title="The best", user ={"username":"Pepega"}))
+def hhh():
+    return(render_template("base_template.html", title="The best"))
 
 @app.route('/index')
 @login_required # setting login restricion on page view
-def chat():
+def index():
     posts = [
     {
         'author': {'username': 'John'},
@@ -42,9 +42,23 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
-    return(render_template("login.html", title="sign In", form = form))
+    #return(render_template("login.html", title="sign In", form = form))
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
