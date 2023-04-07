5dff39a410b933dbfd0b674b062cb9c2be40f6c7
gerasimenkoab <gerasimenko.alexander@gmail.com>
2023-04-07 21:28:03 +0200

    avatar

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer,primary_key = True)
