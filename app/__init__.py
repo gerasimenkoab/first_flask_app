c0e82bcdd4d3912f4a37f4e57416f72476e69b79
gerasimenkoab <gerasimenko.alexander@gmail.com>
2023-04-24 18:23:20 +0200

    error-file-fogging

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app) # creating database instance for app
@@ -13,3 +16,16 @@ migrate = Migrate(app, db) # creating db migration instance for app and db objec
 
 from app import routes,errors
 
+if not app.debug:
+    # if not in the debug mode
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    # creating logfile handler. Type - rotatingFileHandler
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes = 1024, backupCount = 10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathmname)s: %(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
